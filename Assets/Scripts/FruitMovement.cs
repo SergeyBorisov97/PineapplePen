39510b410214e48ec9a324c9f28508118806c604
Sergey Borisov <serg.borisov97@gmail.com>
2022-05-09 17:50:26 +0300

    Add project files.

---
+++ Assets/Scripts/FruitMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitMovement : MonoBehaviour
+{
+    public bool alive = true;
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Move()
+	{
+
+	}
+}
